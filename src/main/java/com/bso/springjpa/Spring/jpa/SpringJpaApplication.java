84ad9573bbbc6e3fc795ea55e65a2ccae0d284bc
Maverick2329 <ing.tomas.imartinez@outlook.com>
2022-10-03 19:06:53 -0500

    Primer Commit

---
+++ src/main/java/com/bso/springjpa/Spring/jpa/SpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.bso.springjpa.Spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaApplication.class, args);
+	}
+
+}
