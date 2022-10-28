d8db751db7d1e94aa62b0618c4c214ac62980b3c
Maverick2329 <ing.tomas.imartinez@outlook.com>
2022-10-27 19:39:49 -0500

    error cors

--- src/main/java/com/bso/springjpa/Spring/jpa/config/ProjectSecurityConfig.java
+++ src/main/java/com/bso/springjpa/Spring/jpa/config/ProjectSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.bso.springjpa.Spring.jpa.config;
 
 import org.springframework.security.core.*;
+
+import java.util.Collections;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -20,6 +23,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.bso.springjpa.Spring.jpa.filter.AuthoritiesLogginAtFilter;
 import com.bso.springjpa.Spring.jpa.filter.AuthoritiesLoggingAfterFilter;
@@ -46,18 +51,38 @@ public class ProjectSecurityConfig  {
 		String[] listAuthenticatedWithBalanceRole = this.authenticatedURLWithBalanceRoles.split(",");
 		String[] listUrlPermited = this.permitedURL.split(",");
 		
-		http.sessionManagement()
-		    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		    .and().authorizeRequests()
+		http.
+		securityContext().requireExplicitSave(false)
+			.and().cors().configurationSource(
+					new CorsConfigurationSource() {
+						
+						@Override
+						public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+							 //TODO Auto-generated method stub
+							CorsConfiguration config = new CorsConfiguration();
+							config.setAllowedOrigins(Collections.singletonList("http://localhost:4200/"));
+							config.setAllowedMethods(Collections.singletonList("*"));
+							config.setAllowCredentials(true);
+							config.setAllowedHeaders(Collections.singletonList("*"));
+							config.setMaxAge(3600L);
+							config.addExposedHeader("Authorization");
+							return config;
+						}
+					})
+			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and().csrf().ignoringAntMatchers(listUrlPermited).csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.and().addFilterAfter(new JWTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+			.addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+		    
+			
+		    .authorizeRequests()
 		    .antMatchers(listAuthenticated).authenticated()
 			.antMatchers(listAuthenticatedWithBalanceRole).hasAuthority("VIEWBALANCE")
 			.antMatchers(listUrlPermited).permitAll()
 			.and().formLogin()
 			.and().httpBasic()
-			//.and().csrf().disable()
-			.and().csrf().ignoringAntMatchers(listUrlPermited).csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-			.and().addFilterAfter(new JWTokenGeneratorFilter(), BasicAuthenticationFilter.class)
-			.addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+			/*.and().csrf().disable()*/
+			
 			//.addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
 			//.addFilterAfter(new AuthoritiesLoggingAfterFilter(), BasicAuthenticationFilter.class)
 			//.addFilterAt(new AuthoritiesLogginAtFilter(), BasicAuthenticationFilter.class)
